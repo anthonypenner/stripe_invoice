cc89fa2d0932db996ce541b2601b787804d87977
gjsherry <gjsherry@gmail.com>
2015-12-04 15:00:44 +0000

    consolidated all migrations to allow changes (e.g. remove :id)

---
+++ db/migrate/20151204144430_create_stripe_invoice_charges.stripe_invoice.rb
@@ -0,0 +1,24 @@
+# This migration comes from stripe_invoice (originally 20140605194026)
+# GJS, consolidated all migrations to allow changes (e.g. remove :id)
+class CreateStripeInvoiceCharges < ActiveRecord::Migration
+  def change
+    create_table :stripe_invoice_charges, :force => true do |t|
+      t.integer :owner_id
+      t.string  :stripe_id
+      t.string  :stripe_refund_id
+      t.integer :parent_invoice_id
+      t.string  :invoice_number
+      t.integer :date
+      t.integer :amount, :default => 0
+      t.string  :currency
+      t.integer :total
+      t.integer :subtotal
+      t.integer :discount
+      t.integer :period_start
+      t.integer :period_end
+      t.text    :json
+
+      t.timestamps
+    end
+  end
+end
