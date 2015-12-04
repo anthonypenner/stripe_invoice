7bfcdc8ed54e8b9b788f6b1cc2cfbea422808f3b
gjsherry <gjsherry@gmail.com>
2015-12-04 15:02:04 +0000

    Delete 20150217203002_rename_invoices_to_charges.rb

--- db/migrate/20150217203002_rename_invoices_to_charges.rb
+++
@@ -1,9 +0,0 @@
-class RenameInvoicesToCharges < ActiveRecord::Migration
-  def change
-    say_with_time "Switching from Stripe-Invoice-objects to Stripe-Charge-objects" do
-      rename_table :stripe_invoice_invoices, :stripe_invoice_charges
-      execute "DELETE FROM stripe_invoice_charges WHERE 1=1"
-    end
-  end
-
-end
