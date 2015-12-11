079fc1aa9d9b64071b1a91577cd5746cf0a92896
Graham Sherry <gjsherry@gmail.com>
2015-12-11 12:03:32 +0000

    :change - change short_date to short for date format

--- app/helpers/stripe_invoice/application_helper.rb
+++ app/helpers/stripe_invoice/application_helper.rb
@@ -1,22 +1,22 @@
 module StripeInvoice
   module ApplicationHelper
-    
+
     def format_timestamp(timestamp)
       dt = timestamp
-      
+
       unless timestamp.is_a? DateTime
         dt = Time.at(timestamp).to_datetime
       end
-      
-      l(dt, :format => :short_date)
+
+      l(dt, :format => :short)
     end
-    
+
     alias_method :ft, :format_timestamp
-    
+
     # returns TRUE if the controller belongs to StripeInvoice
-    # false in all other cases, for convenience when executing filters 
+    # false in all other cases, for convenience when executing filters
     # in the main application
-    def stripe_invoice_controller? 
+    def stripe_invoice_controller?
       is_a? StripeInvoice::ApplicationController
     end
   end
