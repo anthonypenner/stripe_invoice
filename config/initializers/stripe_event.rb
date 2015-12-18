4b7073aa36075980f027b7491cf4deb8391151e1
Graham Sherry <gjsherry@gmail.com>
2015-12-18 14:43:09 +0000

    -update: remove invoice mails on events

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -2,12 +2,14 @@ StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded' do |event|
     # we will create an invoice from this
     charge = StripeInvoice::Charge.create_from_stripe(event.data.object)
-    StripeInvoice::InvoiceMailer.new_invoice(charge).deliver if charge
+
+    # StripeInvoice::InvoiceMailer.new_invoice(charge).deliver if charge
   end
-  
+
   events.subscribe 'charge.refunded' do |event|
     charge = StripeInvoice::Charge.create_from_stripe(event.data.object)
-    StripeInvoice::InvoiceMailer.refund_invoice(charge).deliver if charge
-    
+
+    # StripeInvoice::InvoiceMailer.refund_invoice(charge).deliver if charge
+
   end
 end
